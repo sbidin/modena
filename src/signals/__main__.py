bea80b07013f974c6f1eea7ef5ae0a38ca13c021
Siniša Biđin <sinisa@bidin.eu>
2023-03-17 00:11:52 +0100

    Only read position data at first

--- src/signals/__main__.py
+++ src/signals/__main__.py
@@ -1,9 +1,25 @@
 import sys
+from pathlib import Path
 
-def main():
+import click
+
+from signals.fast5 import Fast5
 
 
+@click.command()
+@click.argument("dataset1")
+@click.argument("dataset2")
+def _main(dataset1: str, dataset2: str) -> None:
+    dataset1, dataset2 = Path(dataset1), Path(dataset2)
+    assert dataset1.exists(), f"no such path: {dataset1}"
+    assert dataset2.exists(), f"no such path: {dataset2}"
+    reads1 = [(f.position_range(), f) for f in Fast5.from_path(dataset1)]
+    reads2 = [(f.position_range(), f) for f in Fast5.from_path(dataset2)]
+    reads1.sort()
+    reads2.sort()
+    print(reads1)
+    print(reads2)
 
 
 if __name__ == "__main__":
-    main()
+    _main()
--- src/signals/fast5.py
+++ src/signals/fast5.py
@@ -15,11 +15,18 @@ log = logging.getLogger("signals.fast5")
 
 @dataclass
 class Fast5:
-    """A parsed FAST5 file."""
+    """A tiny subset of a parsed FAST5 file.
+    
+    This is fetched prior to fetching any signal data. Position data is used to
+    later on pick which files we actually want to process and in which order.
+    """
     path: Path
-    mapped_start: int
-    signal: np.ndarray | None
-    bases: np.ndarray
+    start: int
+    size: int
+
+    def position_range(self) -> tuple[int, int]:
+        """Get the range of positions found within the file."""
+        return self.start, self.start + self.size
 
     @staticmethod
     def from_path(path: Path) -> Iterator["Fast5"]:
@@ -31,13 +38,9 @@ class Fast5:
             if sub.name.lower().endswith(".fast5") and sub.is_file():
                 yield from Fast5._maybe_from_file_path(sub)
 
-    def position_range(self) -> tuple[int, int]:
-        """Get the range of positions found within the file."""
-        return self.mapped_start, self.mapped_start + self.bases.shape[0]
-
     @staticmethod
     def _maybe_from_file_path(path: Path) -> Iterator["Fast5"]:
-        """Attempt parsing a FAST5 file."""
+        """Attempt parsing the basics of a FAST5 file."""
         try:
             log.debug(f"parsing attributes from {path}")
             with h5py.File(path) as f:
@@ -47,40 +50,54 @@ class Fast5:
     
     @staticmethod
     def _parse_file(f: h5py.File, path: Path) -> Iterator["Fast5"]:
-        """Get all file contents that are useful to us."""
-        # Get various useful groups.
+        """Get all basic file contents that are useful to us."""
         tpl = f.get("Analyses/RawGenomeCorrected_000/BaseCalled_template")
         assert tpl, "missing BaseCalled_template"
         aln = tpl.get("Alignment")
         assert aln, "missing Alignment"
         evt = tpl.get("Events")
         assert evt, "missing Events"
-
-        # Get signal modifiers.
-        read_start_rel_to_raw = evt.attrs.get("read_start_rel_to_raw")
-        assert isinstance(read_start_rel_to_raw, np.int64), "missing read_start_rel_to_raw"
-        shift = tpl.attrs.get("shift")
-        assert isinstance(shift, np.float64),  "missing shift"
-        scale = tpl.attrs.get("scale")
-        assert isinstance(scale, np.float64), "missing scale"
-
-        # Get the signal.
-        rds = f.get("Raw/Reads")
-        assert rds, "missing Reads"
-        rdn = list(rds.keys())
-        assert len(rdn) == 1, "not exactly one Read_XYZ found"
-        sgn = rds[rdn[0]]["Signal"]
-        assert sgn, "missing Signal"
-        signal = (sgn[read_start_rel_to_raw:] - shift) / scale
-
-        # Get other useful attributes.
         mapped_start = aln.attrs.get("mapped_start")
         assert isinstance(mapped_start, np.int64), "missing mapped_start"
-        bases = evt["base"]
-        assert isinstance(bases, np.ndarray), "missing base"
+        size = evt.shape[0]
+        assert size > 0, "empty Events"
+        yield Fast5(path=path, start=mapped_start, size=size)
+
+    def signals(self) -> tuple[np.ndarray, np.ndarray] | None:
+        """Get signal and base data."""
+        try:
+            log.debug(f"parsing signal data from {self.path}")
+            with h5py.File(self.path) as f:
+                # Get useful groups.
+                tpl = f.get("Analyses/RawGenomeCorrected_000/BaseCalled_template")
+                assert tpl, "missing BaseCalled_template"
+                aln = tpl.get("Alignment")
+                assert aln, "missing Alignment"
+                evt = tpl.get("Events")
+                assert evt, "missing Events"
+
+                # Get signal modifiers.
+                read_start_rel_to_raw = evt.attrs.get("read_start_rel_to_raw")
+                assert isinstance(read_start_rel_to_raw, np.int64), "missing read_start_rel_to_raw"
+                shift = tpl.attrs.get("shift")
+                assert isinstance(shift, np.float64),  "missing shift"
+                scale = tpl.attrs.get("scale")
+                assert isinstance(scale, np.float64), "missing scale"
+
+                # Get the signal and base data.
+                rds = f.get("Raw/Reads")
+                assert rds, "missing Reads"
+                rdn = list(rds.keys())
+                assert len(rdn) == 1, "not exactly one Read_XYZ found"
+                sgn = rds[rdn[0]]["Signal"]
+                assert sgn, "missing Signal"
+                signal = (sgn[read_start_rel_to_raw:] - shift) / scale
+                bases = evt["base"]
+                assert isinstance(bases, np.ndarray), "missing base"
+                return signal, bases
+        except AssertionError as err:
+            log.warning(f"skipped file {self.path} because {err}")
     
-        yield Fast5(
-            path=path,
-            mapped_start=mapped_start,
-            signal=signal,
-            bases=bases)
+    def __repr__(self) -> str:
+        """Get a simple Fast5 representation for debugging."""
+        return f"Fast5({self.path})"
