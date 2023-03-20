b63d367a34d8cc3e6dbf29cd63fd770469ff43a1
Siniša Biđin <sinisa@bidin.eu>
2023-03-20 23:57:39 +0100

    Remove unnecessary assertions

--- src/signals/fast5.py
+++ src/signals/fast5.py
@@ -68,7 +68,6 @@ class Fast5:
     def _maybe_from_file_path(path: Path) -> Iterator[Fast5]:
         """Attempt parsing the basics of a FAST5 file."""
         try:
-            log.debug(f"parsing attributes from {path}")
             with h5py.File(path) as f:
                 yield from Fast5._parse_file(f, path)
         except AssertionError as err:
@@ -89,47 +88,23 @@ class Fast5:
         assert isinstance(start, np.int64), "missing mapped_start"
         size = evt.shape[0]
         assert size > 0, "empty Events"
-        fast5 = Fast5(path=path, start=start, end=start + size)
-        log.debug(f"parsed file {path} {fast5}")
-        yield fast5
+        yield Fast5(path=path, start=start, end=start + size)
 
     @cached_property
-    def signal(self) -> tuple[np.ndarray, np.ndarray] | None:
-        """Get signal and base data."""
-        try:
-            with h5py.File(self.path) as f:
-                # Get useful groups.
-                tpl = f.get("Analyses/RawGenomeCorrected_000/BaseCalled_template")
-                assert tpl, "missing BaseCalled_template"
-                aln = tpl.get("Alignment")
-                assert aln, "missing Alignment"
-                evt = tpl.get("Events")
-                assert evt, "missing Events"
-
-                # Get signal modifiers.
-                read_start_rel_to_raw = evt.attrs.get("read_start_rel_to_raw")
-                assert isinstance(read_start_rel_to_raw, np.int64), "missing read_start_rel_to_raw"
-                shift = tpl.attrs.get("shift")
-                assert isinstance(shift, np.float64),  "missing shift"
-                scale = tpl.attrs.get("scale")
-                assert isinstance(scale, np.float64), "missing scale"
-
-                # Get the signal and base data.
-                rds = f.get("Raw/Reads")
-                assert rds, "missing Reads"
-                rdn = list(rds.keys())
-                assert len(rdn) == 1, "not exactly one Read_XYZ found"
-                sgn = rds[rdn[0]]["Signal"]
-                assert sgn, "missing Signal"
-                signal = (sgn[read_start_rel_to_raw:] - shift) / scale
-                bases = evt["base"]
-                assert isinstance(bases, np.ndarray), "missing base"
-                lengths = evt["length"]
-                assert isinstance(lengths, np.ndarray), "missing length"
-                return signal, lengths
-        except AssertionError as err:
-            log.debug(f"skipped file {self.path} because {err}")
-            return None, None
+    def signal(self) -> tuple[np.ndarray, np.ndarray]:
+        """Get signal and signal length data."""
+        with h5py.File(self.path) as f:
+            tpl = f.get("Analyses/RawGenomeCorrected_000/BaseCalled_template")
+            evt = tpl.get("Events")
+            read_start_rel_to_raw = evt.attrs.get("read_start_rel_to_raw")
+            shift = tpl.attrs.get("shift")
+            scale = tpl.attrs.get("scale")
+            rds = f.get("Raw/Reads")
+            rdn = list(rds.keys())
+            sgn = rds[rdn[0]]["Signal"]
+            signal = (sgn[read_start_rel_to_raw:] - shift) / scale
+            lengths = evt["length"]
+            return signal, lengths
 
     def __repr__(self) -> str:
         """Get a simple Fast5 representation for debugging."""
