c833fc1d7cdf6349e10ae84fc316f8db4df4fcef
Siniša Biđin <sinisa@bidin.eu>
2023-03-21 23:54:58 +0100

    Test both csv and bedgraph output

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,16 +3,18 @@
 from io import StringIO
 from pathlib import Path
 
+import pytest
 from signals.run import run_on_datasets
 
 
-def test_csv_equals_old_script() -> None:
+@pytest.mark.parametrize("out_format", ("csv", "bedgraph"))
+def test_output_equals_old_script(out_format: str) -> None:
     """CSV output equals output of old script."""
     out = StringIO()
     inp1, inp2 = Path("tests/inp/1"), Path("tests/inp/2")
-    run_on_datasets(inp1, inp2, 1, out, "csv")
+    run_on_datasets(inp1, inp2, 1, out, out_format)
     out = out.getvalue()
-    with open("tests/out.csv") as f:
+    with open(f"tests/out.{out_format}") as f:
         exp = f.read()
     for a, b in zip(out.splitlines(), exp.splitlines(), strict=False):
         assert a == b, "output mismatch"
