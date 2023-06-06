9175210760c662b9350fef65899707b78abe7cd7
Siniša Biđin <sinisa@bidin.eu>
2023-06-06 05:53:08 +0200

    Fix tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -19,7 +19,18 @@ def test_default(suffix: str, resample: int, distsum: bool) -> None:
     """CSV output equals output of old script."""
     out = StringIO()
     inp1, inp2 = Path("tests/inp/dna/1"), Path("tests/inp/dna/2")
-    run_on_datasets(inp1, inp2, "+", None, 1, resample, distsum, out, random_seed=42)
+    run_on_datasets(
+        inp1,
+        inp2,
+        "autodetect",
+        None,
+        None,
+        False,
+        1,
+        resample,
+        distsum,
+        out,
+        random_seed=42)
     out = out.getvalue()
     with open(f"tests/out/dna/out.{suffix}.bed") as f:
         exp = f.read()
