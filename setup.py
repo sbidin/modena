03d7d38e0a57d8acff1aa864f5782928b399ebe0
Siniša Biđin <sinisa@bidin.eu>
2023-06-20 00:41:51 +0200

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nodclust",
-    version="0.0.1",
+    version="0.1.0",
     python_requires=">=3.8",
     package_dir={"": "src"},
     install_requires=[
