58bd07a8dd4436733b3a7856890e14434d81bca7
Siniša Biđin <sinisa@bidin.eu>
2023-06-19 23:02:14 +0200

    Add timer helper

--- src/nodclust/helpers.py
+++ src/nodclust/helpers.py
@@ -1,6 +1,12 @@
 """Provides various helpers."""
 
+import contextlib
+import logging
+from timeit import default_timer
+
 from nodclust.config import Config
+
+log = logging.getLogger("nodclust")
 
 
 def position_within_bounds(
@@ -28,3 +34,12 @@ def position_within_bounds(
             return False
 
     return True
+
+
+@contextlib.contextmanager
+def timer(message: str) -> None:
+    """Output how long a block of code took to execute."""
+    time_from = default_timer()
+    yield
+    seconds = default_timer() - time_from
+    log.debug(f"{message} took {seconds:.3f} seconds")
